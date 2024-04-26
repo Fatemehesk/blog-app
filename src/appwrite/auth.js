e8aeeb405c81751382d14f826727797b758c18b0
Fatemehesk <fatemeh@brandweb.agency>
2024-04-26 11:14:40 +0300

    adding needed forns

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -9,7 +9,7 @@ export class AuthService{
         this.client.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId);
         this.account=new Account(this.client);
     }
-    async createAcount({email,password,name}){
+    async createAccount({email,password,name}){
         try {
          const userAccount =  await this.account.create(ID.unique(),email,password,name);
          if(userAccount){
